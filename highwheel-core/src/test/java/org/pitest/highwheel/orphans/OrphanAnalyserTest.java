4c610009b178da4dddca7fffc3554a55e86b1c30
Natalia Harateh <n.harateh@gmail.com>
2015-07-22 12:46:10 +0100

    Added fillGraph for simpler graph creation

--- highwheel-core/src/test/java/org/pitest/highwheel/orphans/OrphanAnalyserTest.java
+++ highwheel-core/src/test/java/org/pitest/highwheel/orphans/OrphanAnalyserTest.java
@@ -3,7 +3,7 @@ package org.pitest.highwheel.orphans;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -22,30 +22,24 @@ public class OrphanAnalyserTest<V, E> {
   @Test
   public void shouldReturnNoOrphansWhenGraphEmpty() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = Collections.emptyList();
+    graph = fillGraph();
+    entries = fillEntries();
     assertThat(testee.findOrphans(graph, entries)).isEmpty();
   }
 
   @Test
   public void shouldReturnNoOrphansWhenAllAreEntryPoints() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    entries.add("bar");
-    graph.addVertex("foo");
-    graph.addVertex("bar");
+    graph = fillGraph("foo->bar");
+    entries = fillEntries("foo", "bar");
     assertThat(testee.findOrphans(graph, entries)).isEmpty();
   }
 
   @Test
   public void shouldReturnAllOrphansWhenNoEntryPoints() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = Collections.emptyList();
-    graph.addVertex("foo");
-    graph.addVertex("boo");
+    graph = fillGraph("foo->bar");
+    entries = fillEntries();
     assertThat(testee.findOrphans(graph, entries))
         .containsAll(graph.getVertices());
   }
@@ -53,107 +47,94 @@ public class OrphanAnalyserTest<V, E> {
   @Test
   public void shouldReturnNoOrphansWhenAllAreConnectedToAnEntryPoint() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    // foo - > bar
-    // foo -> kro
-    // foo -> car
-    // foo -> moo
-    graph.addEdge(1, "foo", "bar");
-    graph.addEdge(2, "foo", "kro");
-    graph.addEdge(3, "foo", "car");
-    graph.addEdge(4, "foo", "moo");
+    graph = fillGraph("foo->bar", "foo->kro", "foo->car", "foo->moo");
+    entries = fillEntries("foo");
     assertThat(testee.findOrphans(graph, entries)).isEmpty();
   }
 
   @Test
   public void shouldReturnnAnOrphanWhenEntryPointIsDestNotSource() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    graph.addEdge(1, "bar", "foo");
+    graph = fillGraph("bar->foo");
+    entries = fillEntries("foo");
     assertThat(testee.findOrphans(graph, entries)).containsOnly("bar");
   }
 
   @Test
   public void shouldReturnNoOrphansWhenAllEntryPoints() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    entries.add("bar");
-    graph.addEdge(1, "bar", "foo");
+    graph = fillGraph("bar->foo");
+    // System.out.println("shouldReturnNoOrphansWhenAllEntryPoints" + graph);
+    entries = fillEntries("foo", "bar");
     assertThat(testee.findOrphans(graph, entries)).isEmpty();
   }
 
   @Test
   public void shouldReturnnNoOrphansWhenAll2ConnectedToAnEntryPoint() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    graph.addEdge(1, "foo", "bar");
-    graph.addEdge(2, "bar", "moo");
+    graph = fillGraph("foo->bar->moo");
+    entries = fillEntries("foo");
     assertThat(testee.findOrphans(graph, entries)).isEmpty();
   }
 
   @Test
   public void shouldReturnnNoOrphansWhenAll3ConnectedToAnEntryPoint() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    graph.addEdge(1, "foo", "bar");
-    graph.addEdge(2, "bar", "moo");
-    graph.addEdge(3, "bar", "koo");
+    graph = fillGraph("foo->bar->moo", "bar->koo");
+    entries = fillEntries("foo");
     assertThat(testee.findOrphans(graph, entries)).isEmpty();
   }
 
   @Test
   public void shouldReturnnNoOrphansWhenAll3ConnectedToAnEntryPointInACircle() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    // bar -> foo
-    // foo -> moo
-    // bar -> moo
-    graph.addEdge(1, "foo", "bar");
-    graph.addEdge(2, "foo", "moo");
-    graph.addEdge(3, "bar", "moo");
+    graph = fillGraph("foo->bar", "foo->moo", "bar->moo");
+    entries = fillEntries("foo");
     assertThat(testee.findOrphans(graph, entries)).isEmpty();
   }
 
   @Test
   public void shouldReturnNoOrphansWhen2EntryPointsConnectedTo1NonEntryPoint() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    entries.add("bar");
-    graph.addEdge(1, "foo", "moo");
-    graph.addEdge(2, "bar", "moo");
+    graph = fillGraph("foo->moo", "bar->moo");
+    entries = fillEntries("foo", "bar");
     assertThat(testee.findOrphans(graph, entries)).isEmpty();
   }
 
   @Test
   public void shouldReturnOrphansWhenOrphanGroupPresent() {
     testee = new OrphanAnalyser<String, Integer>();
-    graph = new DirectedSparseGraph<String, Integer>();
-    entries = new ArrayList<String>();
-    entries.add("foo");
-    graph.addEdge(1, "foo", "bar");
-    graph.addEdge(2, "bar", "moo");
-    graph.addEdge(3, "bar", "koo");
-    //
-    graph.addEdge(4, "boo", "loo");
-    graph.addEdge(5, "loo", "car");
-    graph.addEdge(6, "loo", "mar");
-    // System.out.println(testee.findOrphans(graph, entries));
+    graph = fillGraph("foo->bar->moo", "bar->koo", "boo->loo->car", "loo->mar");
+    entries = fillEntries("foo");
     assertThat(testee.findOrphans(graph, entries)).containsOnly("boo", "loo",
         "car", "mar");
+  }
+
+  DirectedGraph<String, Integer> fillGraph(String... s) {
+    graph = new DirectedSparseGraph<String, Integer>();
+
+    for (String line : s) {
+
+      List<String> nodes = Arrays.asList(line.split("->"));
+
+      String firstNode = nodes.get(0);
+
+      int edge = graph.getEdgeCount();
+      graph.addEdge(edge, firstNode, nodes.get(1));
+
+      for (int i = 1; i < nodes.size() - 1;) {
+        graph.addEdge(++edge, nodes.get(i), nodes.get(++i));
+      }
+    }
+
+    return graph;
+  }
+
+  List<String> fillEntries(String... e) {
+    entries = new ArrayList<String>();
+    List<String> list = new ArrayList<String>(Arrays.asList(e));
+    entries.addAll(list);
+    return entries;
   }
 
 }
