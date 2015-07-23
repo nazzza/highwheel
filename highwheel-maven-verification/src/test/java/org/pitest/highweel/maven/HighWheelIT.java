f89665859dbf45e80bd691affd4d2876e05d74bb
Natalia Harateh <n.harateh@gmail.com>
2015-07-23 09:31:11 +0100

    Cleaned HighWheelIT

--- highwheel-maven-verification/src/test/java/org/pitest/highweel/maven/HighWheelIT.java
+++ highwheel-maven-verification/src/test/java/org/pitest/highweel/maven/HighWheelIT.java
@@ -29,11 +29,8 @@ public class HighWheelIT {
 
   @Rule
   public TemporaryFolder testFolder = new TemporaryFolderWithOptions(false);
-<<<<<<< HEAD
 
   private Verifier verifier;
-=======
->>>>>>> upstream/master
 
   @Test
   public void shouldProduceAnIndexFile() throws Exception {
